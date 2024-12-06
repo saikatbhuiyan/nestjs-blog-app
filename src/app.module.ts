320240c2f4cc31d855d636d4d1afadb39e8bef1e
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-12-06 21:40:22 +0600

    Update user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
-import { PostsService } from './services/posts.service';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/providers/auth.service';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UsersModule, PostsModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PostsService, AuthService],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/users/providers/users.service.ts
@@ -0,0 +1,38 @@
+import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  constructor() {}
+
+  /*
+   * Method to find all the users
+   */
+  public findAll(
+    getUserParamDto: GetUsersParamDto,
+    limt: number,
+    page: number,
+  ) {
+    return [
+      {
+        firstName: 'John',
+        email: 'john@doe.com',
+      },
+      {
+        firstName: 'Alice',
+        email: 'alice@doe.com',
+      },
+    ];
+  }
+
+  /*
+   * Find a user by ID
+   */
+  public findOneById(id: string) {
+    return {
+      id: 1234,
+      firstName: 'Alice',
+      email: 'alice@doe.com',
+    };
+  }
+}
