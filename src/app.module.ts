152d9a154cb5f2200c830bf10b23f0716ebe523f
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-12-06 17:51:25 +0600

    Create users, posts and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { PostsService } from './services/posts.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/providers/auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostsService, AuthService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // whitelisted additional fields or properties
+      forbidNonWhitelisted: true, // forbid additional fields or properties send errors
+      transform: true, // transforms the incoming request to an instance of the DTO class after validation
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
