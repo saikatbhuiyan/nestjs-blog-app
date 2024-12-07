3fd237b6896fcaf231f66cde5d5ce0859e26128d
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-12-07 12:45:55 +0600

    Added swagger doc in post dto

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
 export class PostsController {
-  constructor(private readonly _postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get('/:userId?')
+  public getPosts(@Param('userId') userId: string) {
+    return this.postsService.findAll(userId);
+  }
+
+  @ApiOperation({
+    summary: 'Creates a new post for the blog.',
+  })
+  @ApiResponse({
+    status: 201,
+    description:
+      'You get a success 201 response if the post is created successfully',
+  })
+  @Post()
+  public createPost(@Body() createPostDto: CreatePostDto) {
+    console.log(createPostDto);
+  }
 }
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly usersService: UsersService) {}
+
+  public findAll(userId: string) {
+    const user = this.usersService.findOneById(userId);
+
+    return [
+      {
+        user: user,
+        title: 'Test Tile',
+        content: 'Test Content',
+      },
+      {
+        user: user,
+        title: 'Test Tile 2',
+        content: 'Test Content 2',
+      },
+    ];
+  }
+}
